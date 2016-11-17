4d8a761cdec8fc245e2dfd70416eb6d1b75e44ce
chetan verma <verma.chetan@gmail.com>
2016-11-17 23:45:29 +0530

    Cleaned up the demo code

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,35 +7,19 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
+import React from 'react';
+
 /* eslint-disable global-require */
 
 // The top-level (parent) route
 export default {
-
   path: '/',
 
-  // Keep in mind, routes are evaluated in order
-  children: [
-    require('./home').default,
-    require('./contact').default,
-    require('./login').default,
-    require('./register').default,
-    require('./admin').default,
-
-    // Wildcard routes, e.g. { path: '*', ... } (must go last)
-    require('./content').default,
-    require('./notFound').default,
-  ],
-
-  async action({ next }) {
-    // Execute each child route until one of them return the result
-    const route = await next();
-
-    // Provide default values for title, description etc.
-    route.title = `${route.title || 'Untitled Page'} - www.reactstarterkit.com`;
-    route.description = route.description || '';
-
-    return route;
+  async action() {
+    return {
+      title: 'title',
+      component: <div>Home</div>
+    }
   },
 
 };
